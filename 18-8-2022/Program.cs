935e73a19e39a3f0f652c85ce898bb339a5ab52a
LENOVO <LENOVO@DESKTOP-JJRNDTI>
2022-08-18 12:49:30 +0530

    Add project files.

---
+++ 18-8-2022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_8_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object o;
+            int i = 100;
+            Type t1 = i.GetType();
+            Console.WriteLine(t1.Name);
+            Console.WriteLine(t1.FullName);
+            Console.ReadLine();
+        } 
+    }
+}
